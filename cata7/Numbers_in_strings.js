afa78800170078f78cbdb9f83cab295469850343
Danyil <102794375+dofaqq@users.noreply.github.com>
2023-09-16 21:03:49 +0300

    Create Numbers_in_strings.js

---
+++ cata7/Numbers_in_strings.js
@@ -0,0 +1,7 @@
+/*
+In this Kata, you will be given a string that has lowercase letters and numbers. Your task is to compare the number groupings and return the largest number. Numbers will not have leading zeros.
+*/
+
+function solve(s){
+  return Math.max(...s.match(/\d+/g));
+};
